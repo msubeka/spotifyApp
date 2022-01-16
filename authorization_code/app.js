72eb87f2acbc0587050ee615a5aafb717146b656
Subeka M <msubeka2@gmail.com>
2022-01-15 23:39:04 -0500

    Initial

--- authorization_code/app.js
+++ authorization_code/app.js
@@ -13,9 +13,9 @@ var cors = require('cors');
 var querystring = require('querystring');
 var cookieParser = require('cookie-parser');
 
-var client_id = 'CLIENT_ID'; // Your client id
-var client_secret = 'CLIENT_SECRET'; // Your secret
-var redirect_uri = 'REDIRECT_URI'; // Your redirect uri
+var client_id = '90d08aa770e0472299f4785793af2492'; // Your client id
+var client_secret = 'CLIENT-SECRET'; // Your secret
+var redirect_uri = 'http://localhost:8888/callback'; // Your redirect uri
 
 /**
  * Generates a random string containing numbers and letters
